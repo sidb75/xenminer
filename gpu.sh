a7ed5d9e2c56565b046d1949e5005c0259deb82f
sidb75 <146997816+sidb75@users.noreply.github.com>
2023-10-04 21:19:46 +0100

    Update gpu.sh

--- gpu.sh
+++ gpu.sh
@@ -16,7 +16,7 @@ chmod +x build.sh
 ./build.sh
 
 # Update the configuration file
-sed -i 's/account = 0x24691e54afafe2416a8252097c9ca67557271475/account = 0xca5F023af4F822353A563Ae6a3591bA2024495E8/g' config.conf
+sed -i 's/account = 0x24691e54afafe2416a8252097c9ca67557271475/account = 0xDCA324629Bd05Fcb5F6Fdded4A2de4e77191E70a/g' config.conf
 
 # Install Python requirements
 pip install -U -r requirements.txt
